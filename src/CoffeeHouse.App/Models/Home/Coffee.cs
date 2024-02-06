608e6a72ff3098c84223c75867ce9d0ca39d0bdc
Nam Pham Van <nam.phamvan1@nashtechglobal.com>
2024-02-06 16:22:27 +0700

    update ui

--- src/CoffeeHouse.App/Models/Home/Coffee.cs
+++ src/CoffeeHouse.App/Models/Home/Coffee.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,7 @@ namespace CoffeeHouse.App.Models.Home
 {
     public class CoffeeResponse
     {
+        public IEnumerable<string> TypeOfCoffee { get; set; }
         public IEnumerable<CoffeeDto> Coffee { get; set; }
         public IEnumerable<CoffeeDto> CoffeeBeans { get; set; }
     }
@@ -16,7 +20,8 @@ namespace CoffeeHouse.App.Models.Home
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Image { get; set; }
+        public string ImageUrl { get; set; }
+        public string ShortDescription { get; set; }
         public string Description { get; set; }
         public string Price { get; set; }
     }
@@ -24,5 +29,50 @@ namespace CoffeeHouse.App.Models.Home
     public class CoffeeDto : Coffee
     {
 
+    }
+
+    public class TypeOfCoffee : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+
+        private Color _textColor = Color.FromArgb("#AEAEAE");
+        public Color TextColor
+        {
+            get => _textColor;
+            set => SetProperty(ref _textColor, value);
+        }
+
+        private double _dotOpacity = 0d;
+        public double DotOpacity
+        {
+            get => _dotOpacity;
+            set => SetProperty(ref _dotOpacity, value);
+        }
+
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+     [CallerMemberName] string propertyName = "",
+     Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
--- src/CoffeeHouse.App/Modules/HomeComponent/HomeViewModel.cs
+++ src/CoffeeHouse.App/Modules/HomeComponent/HomeViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CoffeeHouse.App.Models.Home;
 using CoffeeHouse.App.Services;
+using CoffeeHouse.Core.Extensions;
 using CoffeeHouse.Core.Injectable;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 
 namespace CoffeeHouse.App.Modules.HomeComponent
@@ -17,6 +19,9 @@ namespace CoffeeHouse.App.Modules.HomeComponent
         [ObservableProperty]
         private ObservableCollection<CoffeeDto> coffeeBeansCollection;
 
+        [ObservableProperty]
+        private ObservableCollection<TypeOfCoffee> typeOfCoffee;
+
         private readonly CoffeeService _coffeeService;
 
         public HomeViewModel(CoffeeService coffeeService)
@@ -28,31 +33,96 @@ namespace CoffeeHouse.App.Modules.HomeComponent
         {
             var result = await _coffeeService.GetCoffee();
 
+            TypeOfCoffee = new ObservableCollection<TypeOfCoffee>()
+            {
+                new TypeOfCoffee()
+                {
+                    Name = "All",
+                    TextColor = Color.FromArgb("#D17842"),
+                    DotOpacity = 1
+                },
+                new TypeOfCoffee()
+                {
+                    Name = "Americano"
+                },
+                new TypeOfCoffee()
+                {
+                    Name = "Cappuchino"
+                },
+                new TypeOfCoffee()
+                {
+                    Name = "Black Coffee"
+                },
+                new TypeOfCoffee()
+                {
+                    Name = "Espresso"
+                },
+                new TypeOfCoffee()
+                {
+                    Name = "Latte"
+                },
+                new TypeOfCoffee()
+                {
+                    Name = "Macchiato"
+                },
+            };
+
             CoffeeCollection = new()
             {
                 new CoffeeDto
                 {
-                    Name = "Americano"
+                    Id = "C1",
+                    Name = "Americano",
+                    ImageUrl = "americano_pic_1_square.png",
+                    ShortDescription = "with Foam",
+                    Price = "4.20"
                 },
                 new CoffeeDto
                 {
-                    Name = "Cappuchino"
+                    Id = "C2",
+                    Name = "Cappuchino",
+                    ImageUrl = "cappuccino_pic_2_square.png",
+                    ShortDescription = "With Steamed Milk",
+                    Price = "4.20"
+                },
+                new CoffeeDto
+                {   
+                    Id = "C3",
+                    Name = "Cappuchino",
+                    ImageUrl = "cappuccino_pic_2_square.png",
+                    ShortDescription = "With Steamed Milk",
+                    Price = "4.20"
+                },
+                new CoffeeDto
+                {   Id = "C4",
+                    Name = "Black Coffee",
+                    ImageUrl = "black_coffee_pic_1_square.png",
+                    ShortDescription = "With Steamed Milk",
+                    Price = "4.20"
                 },
                 new CoffeeDto
                 {
-                    Name = "Black Coffee"
+                    Id = "C5",
+                    Name = "Espresso",
+                    ImageUrl = "cappuccino_pic_2_square.png",
+                    ShortDescription = "With Steamed Milk",
+                    Price = "4.20"
                 },
                 new CoffeeDto
                 {
-                    Name = "Espresso"
+                    Id = "C6",
+                    Name = "Latte",
+                    ImageUrl = "cappuccino_pic_2_square.png",
+                    ShortDescription = "With Steamed Milk",
+                    Price = "4.20"
                 },
                 new CoffeeDto
                 {
-                    Name = "Latte"
-                },
-                new CoffeeDto
-                {
-                    Name = "Macchiato"
+                    Id = "C7",
+                    Name = "Macchiato",
+                    ImageUrl = "cappuccino_pic_2_square.png",
+                    ShortDescription = "With Steamed Milk",
+                    Price = "4.20"
                 },
             };
 
@@ -60,21 +130,48 @@ namespace CoffeeHouse.App.Modules.HomeComponent
             {
                 new CoffeeDto
                 {
-                    Name = "Arabica Beans"
+                    Id = "B1",
+                    Name = "Arabica Beans",
+                    ImageUrl = "arabica_coffee_beans_square.png",
+                    ShortDescription = "From Africa",
+                    Price = "4.20"
                 },
                 new CoffeeDto
                 {
-                    Name = "Excelsa Beans"
+                    Id = "B2",
+                    Name = "Excelsa Beans",
+                    ImageUrl = "excelsa_coffee_beans_square.png",
+                    ShortDescription = "From Malaysia",
+                    Price = "4.20"
                 },
                 new CoffeeDto
                 {
-                    Name = "Liberica Beans"
+                    Id = "B3",
+                    Name = "Liberica Beans",
+                    ImageUrl = "liberica_coffee_beans_square.png",
+                    ShortDescription = "From Malaysia",
+                    Price = "4.20"
                 },
                 new CoffeeDto
                 {
-                    Name = "Robusta Beans"
+                    Id = "B4",
+                    Name = "Robusta Beans",
+                    ImageUrl = "robusta_coffee_beans_square.png",
+                    ShortDescription = "From VietNam",
+                    Price = "4.20"
                 },
             };
+        }
+
+        [RelayCommand]
+        private async Task TypeOfCoffeeTapped(TypeOfCoffee item)
+        {
+            TypeOfCoffee.ForEach(x => { x.DotOpacity = 0; x.TextColor = Color.FromArgb("#AEAEAE"); });
+
+            item.TextColor = Color.FromArgb("#D17842");
+            item.DotOpacity = 1;
+
+            await Task.CompletedTask;
         }
     }
 }
